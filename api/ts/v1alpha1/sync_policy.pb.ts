05a93fc288d830cc9c08abc94a0ab5a65352398b
hai.tang <hai.tang@daocloud.io>
2026-03-18 17:38:59 +0800

    update replcation proto to sync policy

---
+++ api/ts/v1alpha1/sync_policy.pb.ts
@@ -0,0 +1,205 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../fetch.pb"
+import * as MatrixhubV1alpha1Registry from "./registry.pb"
+import * as MatrixhubV1alpha1Utils from "./utils.pb"
+
+type Absent<T, K extends keyof T> = { [k in Exclude<keyof T, K>]?: undefined };
+type OneOf<T> =
+  | { [k in keyof T]?: undefined }
+  | (
+    keyof T extends infer K ?
+      (K extends string & keyof T ? { [k in K]: T[K] } & Absent<T, K>
+        : never)
+    : never);
+
+export enum SyncPolicyType {
+  SYNC_POLICY_TYPE_UNSPECIFIED = "SYNC_POLICY_TYPE_UNSPECIFIED",
+  SYNC_POLICY_TYPE_PULL_BASE = "SYNC_POLICY_TYPE_PULL_BASE",
+  SYNC_POLICY_TYPE_PUSH_BASE = "SYNC_POLICY_TYPE_PUSH_BASE",
+}
+
+export enum ResourceType {
+  RESOURCE_TYPE_UNSPECIFIED = "RESOURCE_TYPE_UNSPECIFIED",
+  RESOURCE_TYPE_MODEL = "RESOURCE_TYPE_MODEL",
+  RESOURCE_TYPE_DATASET = "RESOURCE_TYPE_DATASET",
+  RESOURCE_TYPE_ALL = "RESOURCE_TYPE_ALL",
+}
+
+export enum TriggerType {
+  TRIGGER_TYPE_UNSPECIFIED = "TRIGGER_TYPE_UNSPECIFIED",
+  TRIGGER_TYPE_MANUAL = "TRIGGER_TYPE_MANUAL",
+  TRIGGER_TYPE_SCHEDULED = "TRIGGER_TYPE_SCHEDULED",
+}
+
+export enum SyncTaskStatus {
+  SYNC_TASK_STATUS_UNSPECIFIED = "SYNC_TASK_STATUS_UNSPECIFIED",
+  SYNC_TASK_STATUS_RUNNING = "SYNC_TASK_STATUS_RUNNING",
+  SYNC_TASK_STATUS_SUCCEEDED = "SYNC_TASK_STATUS_SUCCEEDED",
+  SYNC_TASK_STATUS_FAILED = "SYNC_TASK_STATUS_FAILED",
+  SYNC_TASK_STATUS_STOPPED = "SYNC_TASK_STATUS_STOPPED",
+}
+
+export type PullBasePolicy = {
+  sourceRegistryId?: number
+  resourceName?: string
+  resourceTypes?: ResourceType[]
+  targetResourceName?: string
+  sourceRegistry?: MatrixhubV1alpha1Registry.Registry
+}
+
+export type PushBasePolicy = {
+  resourceName?: string
+  resourceTypes?: ResourceType[]
+  targetRegistryId?: number
+  targetResourceName?: string
+  targetRegistry?: MatrixhubV1alpha1Registry.Registry
+}
+
+
+type BaseSyncPolicyItem = {
+  id?: number
+  name?: string
+  description?: string
+  policyType?: SyncPolicyType
+  triggerType?: TriggerType
+  bandwidth?: string
+  isOverwrite?: boolean
+  isDisabled?: boolean
+}
+
+export type SyncPolicyItem = BaseSyncPolicyItem
+  & OneOf<{ pullBasePolicy: PullBasePolicy; pushBasePolicy: PushBasePolicy }>
+
+
+type BaseCreateSyncPolicyRequest = {
+  name?: string
+  description?: string
+  policyType?: SyncPolicyType
+  triggerType?: TriggerType
+  bandwidth?: string
+  isOverwrite?: boolean
+}
+
+export type CreateSyncPolicyRequest = BaseCreateSyncPolicyRequest
+  & OneOf<{ pullBasePolicy: PullBasePolicy; pushBasePolicy: PushBasePolicy }>
+
+export type CreateSyncPolicyResponse = {
+  syncPolicy?: SyncPolicyItem
+}
+
+
+type BaseUpdateSyncPolicyRequest = {
+  syncPolicyId?: number
+  name?: string
+  description?: string
+  triggerType?: TriggerType
+  bandwidth?: string
+  isOverwrite?: boolean
+  isDisabled?: boolean
+}
+
+export type UpdateSyncPolicyRequest = BaseUpdateSyncPolicyRequest
+  & OneOf<{ pullBasePolicy: PullBasePolicy; pushBasePolicy: PushBasePolicy }>
+
+export type UpdateSyncPolicyResponse = {
+  syncPolicy?: SyncPolicyItem
+}
+
+export type DeleteSyncPolicyRequest = {
+  syncPolicyId?: number
+}
+
+export type DeleteSyncPolicyResponse = {
+  syncPolicy?: SyncPolicyItem
+}
+
+export type ListSyncPoliciesRequest = {
+  page?: number
+  pageSize?: number
+  search?: string
+}
+
+export type ListSyncPoliciesResponse = {
+  syncPolicies?: SyncPolicyItem[]
+  pagination?: MatrixhubV1alpha1Utils.Pagination
+}
+
+export type GetSyncPolicyRequest = {
+  syncPolicyId?: number
+}
+
+export type GetSyncPolicyResponse = {
+  syncPolicy?: SyncPolicyItem
+}
+
+export type CreateSyncTaskRequest = {
+  syncPolicyId?: number
+}
+
+export type CreateSyncTaskResponse = {
+  id?: number
+}
+
+export type SyncTask = {
+  id?: number
+  syncPolicyId?: number
+  triggerType?: TriggerType
+  status?: SyncTaskStatus
+  startedTimestamp?: string
+  completedTimestamp?: string
+  totalItems?: string
+  successfulItems?: string
+}
+
+export type ListSyncTasksRequest = {
+  syncPolicyId?: number
+  page?: number
+  pageSize?: number
+  search?: string
+}
+
+export type ListSyncTasksResponse = {
+  syncTasks?: SyncTask[]
+  pagination?: MatrixhubV1alpha1Utils.Pagination
+}
+
+export type StopSyncTaskRequest = {
+  syncPolicyId?: number
+  syncTaskId?: number
+}
+
+export type StopSyncTaskResponse = {
+  syncTask?: SyncTask
+}
+
+export class SyncPolicy {
+  static ListSyncPolicies(req: ListSyncPoliciesRequest, initReq?: fm.InitReq): Promise<ListSyncPoliciesResponse> {
+    return fm.fetchReq<ListSyncPoliciesRequest, ListSyncPoliciesResponse>(`/api/v1alpha1/sync-policies?${fm.renderURLSearchParams(req, [])}`, {...initReq, method: "GET"})
+  }
+  static GetSyncPolicy(req: GetSyncPolicyRequest, initReq?: fm.InitReq): Promise<GetSyncPolicyResponse> {
+    return fm.fetchReq<GetSyncPolicyRequest, GetSyncPolicyResponse>(`/api/v1alpha1/sync-policies/${req["syncPolicyId"]}?${fm.renderURLSearchParams(req, ["syncPolicyId"])}`, {...initReq, method: "GET"})
+  }
+  static CreateSyncPolicy(req: CreateSyncPolicyRequest, initReq?: fm.InitReq): Promise<CreateSyncPolicyResponse> {
+    return fm.fetchReq<CreateSyncPolicyRequest, CreateSyncPolicyResponse>(`/api/v1alpha1/sync-policies`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static UpdateSyncPolicy(req: UpdateSyncPolicyRequest, initReq?: fm.InitReq): Promise<UpdateSyncPolicyResponse> {
+    return fm.fetchReq<UpdateSyncPolicyRequest, UpdateSyncPolicyResponse>(`/api/v1alpha1/sync-policies/${req["syncPolicyId"]}`, {...initReq, method: "PUT", body: JSON.stringify(req, fm.replacer)})
+  }
+  static DeleteSyncPolicy(req: DeleteSyncPolicyRequest, initReq?: fm.InitReq): Promise<DeleteSyncPolicyResponse> {
+    return fm.fetchReq<DeleteSyncPolicyRequest, DeleteSyncPolicyResponse>(`/api/v1alpha1/sync-policies/${req["syncPolicyId"]}`, {...initReq, method: "DELETE"})
+  }
+  static CreateSyncTask(req: CreateSyncTaskRequest, initReq?: fm.InitReq): Promise<CreateSyncTaskResponse> {
+    return fm.fetchReq<CreateSyncTaskRequest, CreateSyncTaskResponse>(`/api/v1alpha1/sync-policies/${req["syncPolicyId"]}/sync-tasks`, {...initReq, method: "POST"})
+  }
+  static ListSyncTasks(req: ListSyncTasksRequest, initReq?: fm.InitReq): Promise<ListSyncTasksResponse> {
+    return fm.fetchReq<ListSyncTasksRequest, ListSyncTasksResponse>(`/api/v1alpha1/sync-policies/${req["syncPolicyId"]}/sync-tasks?${fm.renderURLSearchParams(req, ["syncPolicyId"])}`, {...initReq, method: "GET"})
+  }
+  static StopSyncTask(req: StopSyncTaskRequest, initReq?: fm.InitReq): Promise<StopSyncTaskResponse> {
+    return fm.fetchReq<StopSyncTaskRequest, StopSyncTaskResponse>(`/api/v1alpha1/sync-policies/${req["syncPolicyId"]}/sync-tasks/${req["syncTaskId"]}/stop`, {...initReq, method: "POST"})
+  }
+}
